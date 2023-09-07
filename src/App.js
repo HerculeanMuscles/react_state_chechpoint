0441526e05847cd951c8003176ceca7ea794e246
dobbysock69 <103392445+dobbysock69@users.noreply.github.com>
2023-09-07 16:35:32 +0100

    added the timeinterval for the mount

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import logo from "./logo.svg";
+import Card from "react-bootstrap/Card";
 import "./App.css";
 
 class App extends Component {
@@ -8,34 +10,75 @@ class App extends Component {
 			person: {
 				fullName: "Fahri",
 				bio: "A web developer",
-				imgSrc: "image-url.jpg",
+				imgSrc: logo,
 				profession: "Software Engineer",
 			},
-			shows: true,
+			shows: false,
+			timeSinceMount: 0,
+			personDisplayTime: null, // Store the time when the Person component is displayed
 		};
+		this.intervalId = null; // Initialize intervalId to null
 	}
 
 	// Function to toggle the 'shows' state
 	toggleShow = () => {
+		if (!this.state.shows) {
+			this.setState({
+				personDisplayTime: new Date(), // Store the time when the Person component is displayed
+			});
+		}
 		this.setState((prevState) => ({
 			shows: !prevState.shows,
 		}));
 	};
 
+	// Function to update timeSinceMount
+	updateTimeInterval = () => {
+		if (this.state.personDisplayTime) {
+			const currentTime = new Date();
+			const timeDifference = Math.round(
+				(currentTime - this.state.personDisplayTime) / 1000
+			); // Round to the nearest whole number
+			this.setState({
+				timeSinceMount: timeDifference,
+			});
+		}
+	};
+
+	// Lifecycle method: componentDidMount
+	componentDidMount() {
+		this.intervalId = setInterval(this.updateTimeInterval, 1000); // Update time interval every second
+	}
+
+	// Lifecycle method: componentWillUnmount
+	componentWillUnmount() {
+		clearInterval(this.intervalId); // Clear the interval when the component is unmounted
+	}
+
 	render() {
-		const { person, shows } = this.state;
+		const { person, shows, timeSinceMount } = this.state;
 
 		return (
 			<div className="App">
 				<button onClick={this.toggleShow}>Toggle Profile</button>
+				<br />
 				{shows && (
-					<>
-						<img src={person.imgSrc} alt={person.fullName} />
-						<h1>{person.fullName}</h1>
-						<p>{person.bio}</p>
-						<p>{person.profession}</p>
-					</>
+					<div className="Person">
+						<Card style={{ width: "18rem" }}>
+							<Card.Img
+								variant="top"
+								src={person.imgSrc}
+								alt={person.fullName}
+							/>
+							<Card.Body>
+								<Card.Title>{person.fullName}</Card.Title>
+								<Card.Text>{person.bio}</Card.Text>
+								<Card.Text>{person.profession}</Card.Text>
+							</Card.Body>
+						</Card>
+					</div>
 				)}
+				<p>Time since mount: {timeSinceMount} seconds</p>
 			</div>
 		);
 	}
